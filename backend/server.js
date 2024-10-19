e457df4e60d1151a684705446c20c5ab07f258cb
visak14 <121089328+visak14@users.noreply.github.com>
2024-10-19 17:57:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,13 @@ app.use(express.json());
 app.use(cors())
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+});
+
 
 
 
